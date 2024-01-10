36fe4dc90cf6aec67d4979bc5322cf343b77566e
Al-mudather <wollof8@gmail.com>
2024-01-10 12:12:39 +0300

    adding-the-send-message-section-design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,61 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-        navToggle = document.getElementById('nav-toggle'),
-        navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*=============== MENU Show ===============*/
 /** Validate if constant exists */
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /*=============== MENU Hidden ===============*/
 /** Validate if constant exists */
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    //When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-    
-}
+  const navMenu = document.getElementById("nav-menu");
+  //When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
 
-navLink.forEach( n => n.addEventListener('click', linkAction) )
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== SHADOW HEADER ===============*/
 const shadowHeader = () => {
-    const header = document.getElementById('header')
-    // When the scroll is grater than 50 viewport height , add the shadow-header class
-    this.scrollY >= 50 ? header.classList.add('shadow-header')
-    : header.classList.remove('shadow-header')
-    // console.log(header.classList)
-}
-window.addEventListener('scroll', shadowHeader)
+  const header = document.getElementById("header");
+  // When the scroll is grater than 50 viewport height , add the shadow-header class
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+  // console.log(header.classList)
+};
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
 
+const sendEmail = (e) => {
+    e.preventDefault()
 
-/*=============== SHOW SCROLL UP ===============*/ 
+    // serviceID - templateID - #form - publicKey
+}
 
+contactForm.addEventListener('submit', sendEmail)
+
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
